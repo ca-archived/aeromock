e324b050b20b933adb90c3c4bdd81d2c0d52c8fa
stormcat24 <a.yamada24@gmail.com>
2014-09-12 18:58:54 +0900

    WIP

---
+++ aeromock-view/gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+
+gulp.task("sandbox", function(callback) {
+  console.log("sandbox");
+});
+
