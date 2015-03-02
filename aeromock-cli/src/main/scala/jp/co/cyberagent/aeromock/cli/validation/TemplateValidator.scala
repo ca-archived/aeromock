fa3d2bdc810bd29d6ffd63be867ee8dfbb0eb45b
stormcat24 <a.yamada24@gmail.com>
2015-03-02 21:37:11 +0900

    [ci skip][WIP]

--- aeromock-cli/src/main/scala/jp/co/cyberagent/aeromock/cli/validation/TemplateValidator.scala
+++ aeromock-cli/src/main/scala/jp/co/cyberagent/aeromock/cli/validation/TemplateValidator.scala
@@ -1,8 +1,8 @@
 package jp.co.cyberagent.aeromock.cli.validation
 
-import java.nio.file.Path
+import java.nio.file.{Files, Path}
 
-import jp.co.cyberagent.aeromock.config.{Naming, Data, Template}
+import jp.co.cyberagent.aeromock.config.{Naming, Data, Template, Test}
 import jp.co.cyberagent.aeromock.core.http.Endpoint
 import jp.co.cyberagent.aeromock.data.DataFileService
 import jp.co.cyberagent.aeromock.helper._
@@ -16,13 +16,16 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success}
 
+import scalaz._
+import Scalaz._
+
 /**
  *
  * @author stormcat24
  */
 class TemplateValidator(implicit inj: Injector) extends AnyRef with Injectable {
 
-  def validate(template: Template, data: Data, naming: Naming): Unit = {
+  def validate(template: Template, data: Data, naming: Naming, test: Test): Unit = {
     // TODO 拡張子取るためだけに取るのイケてない
     val templateService = inject[Option[TemplateService]].get
 
@@ -34,26 +37,30 @@ class TemplateValidator(implicit inj: Injector) extends AnyRef with Injectable {
         val uri = templatePath.getRelativePath(template.root).withoutExtension
 
         val candidates = DataFileService.getRelatedDataFiles(Endpoint(uri.toString))
-        // URL構築
-        candidates.map { dataFile =>
-//          dataFile.
-          val request = createUrl(requestRoot, uri, dataFile)
-          val future = Http(request > as.String)
-          future onComplete {
-            case Success(result) => println(result)
-            case Failure(f) => {
-              throw f
-            }
-          }
-//          val result = Await.result(future, Duration.Inf)
-//          println(result)
+        val tests = candidates.map { dataFile =>
 
-          // HTTP client
+          val request = createUrl(requestRoot, uri, dataFile)
+          val future = Http(request OK as.String)
+          val testResult = \/.fromTryCatchNonFatal(Await.result(future, Duration.Inf)) match {
+            case -\/(e) => {
+              val cause = e.getCause.asInstanceOf[StatusCode]
+              (false, cause.code)
+            }
+            case \/-(result) => (true, 200)
+          }
+          Map(
+            "request_url" -> request.toRequest.getUrl,
+            "data_file" -> dataFile.path.toString,
+            "status" -> testResult._2,
+            "result" -> testResult._1
+          )
         }
-        // Request
-        // 結果
-        // JSON構築
-        // テスト１件ずつJSONだした方がよさげ
+
+        val result = Map(
+          "template_uri" -> uri.toString,
+          "tests" -> tests
+        )
+        writeJson(result, test.reportRoot)
       }
     }
   }
@@ -63,5 +70,19 @@ class TemplateValidator(implicit inj: Injector) extends AnyRef with Injectable {
       case DataFile(Some(id), path, method) => s"${urlRoot}${requestUri}?_dataid=${id}"
       case DataFile(None, path, method) => s"${urlRoot}${requestUri}"
     }).setHeader("User-Agent", "Aeromock Test Job")
+  }
+
+  private def writeJson(testResult: Map[String, Any], reportRoot: Path): Unit = {
+
+    import org.json4s._
+    import org.json4s.native.Serialization
+    import org.json4s.native.Serialization.write
+    implicit val formats = Serialization.formats(NoTypeHints)
+
+    val templateReportRoot = reportRoot / "template"
+    Files.createDirectories(templateReportRoot)
+
+    val jsonString = write(testResult)
+    println(jsonString)
   }
 }
